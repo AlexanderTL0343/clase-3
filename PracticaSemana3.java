94dc7c1aa263dcd31a0a29df00565081c1a0a1a2
AlexanderTL0343 <145624722+AlexanderTL0343@users.noreply.github.com>
2023-09-27 20:26:44 -0600

    Add files via upload

---
+++ PracticaSemana3.java
@@ -0,0 +1,75 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica.semana.pkg3;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alext
+ */
+public class PracticaSemana3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         /*
+    ejercicio 1
+    */
+         /*
+     String resultado="";
+     for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 10; j++) {
+            resultado=resultado + "@";    
+            }
+        resultado=resultado + "\n";
+        }
+     System.out.println(resultado);
+      */
+     /*
+    ejercicio 2
+    */
+    /*
+    int numero;
+    String lectura;
+    lectura=JOptionPane.showInputDialog("Digite su Numero:");
+    numero = Integer.parseInt(lectura);
+    int numero=, suma=0;
+    while (i<=10) {
+        suma +=i; 
+            ++i; 
+        }
+    System.out.println(“La sumatoria es: ”+suma);
+     */
+    /*
+    ejercicio 3
+    */
+    /*
+    int numero;
+    String lectura;
+    lectura=JOptionPane.showInputDialog("Digite su Numero:");
+    numero = Integer.parseInt(lectura);
+    String resultado="";
+    for (var i = 0; i < numero; i++) {
+        for (int j = 0;  numero < i; j++) {
+        resultado=resultado + "*";    
+            }
+        resultado=resultado + "\n";
+        }
+     System.out.println(resultado);
+     */
+     /*
+    ejercicio 4
+    */
+     
+      /*
+    ejercicio 5
+    */
+    
+    }
+   
+    
+}
